8610817b00cdab5856813f4c91b92aaac5ca3295
kaua <Borgeskaua296@gmail.com>
2025-09-24 10:26:21 +0100

    .

--- testFuncion.js
+++ testFuncion.js
@@ -1,13 +1,5 @@
-import * as readline from 'node:readline'
-
 function main(){
-    
-
     console.log("hello World")
-    console.log("Indique o primeiro numero para soma:5");
-    
-    console.log((("Indique o segundo numero para a soma:20")));
-
     return("A soma é "+ some(5,20))
 }
 
